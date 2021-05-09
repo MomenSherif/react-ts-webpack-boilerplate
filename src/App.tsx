c593d33a884348a596e53ccd56aa6bb8a628c239
Momen Sherif <momensherif.2019@gmail.com>
2021-05-09 21:07:33 +0200

    ADD: Webpack handle Css & assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
+
+import IMAGE from './react.png';
+import LOGO from './logo.svg';
+
+import './style.css';
+
 export const App: React.FC = () => (
-  <h1>React Typescript Webpack Starter Template 📦🐱‍👤</h1>
+  <div>
+    <h1>React Typescript Webpack Starter Template 📦🐱‍👤</h1>
+    <img src={IMAGE} alt="" width="100" />
+    <img src={LOGO} alt="" width="100" />
+  </div>
 );
